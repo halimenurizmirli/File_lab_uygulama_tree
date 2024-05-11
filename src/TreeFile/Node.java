0d79a90effbff69c751db52a2208ff5e3cd861f2
dell laptop <ldell.laptop.2021@outlook.com>
2024-05-11 14:19:31 +0300

    Node class'ı ayrıldı. fonksiyonlar static yapıldı. fonksiyon parametreleri değiştirildi.

---
+++ src/TreeFile/Node.java
@@ -0,0 +1,201 @@
+package TreeFile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class Node {
+	String name = null;
+	List<Node> children;
+
+	Node(String name) {
+		this.name = name;
+		this.children = new ArrayList<Node>();
+	}
+
+	static void addChildren(String childName, Node node) {
+		String[] path = childName.split("/");
+		Node current = node;
+		for (int i = 1; i < path.length; i++) {
+			String directory = path[i];
+			Node child = null;
+			for (Node existingChild : current.children) {
+				if (existingChild.name.equals(directory)) {
+					child = existingChild;
+					break;
+				}
+			}
+			if (child == null) {
+				child = new Node(directory);
+				current.children.add(child);
+				Collections.sort(current.children, new ComparebyName());
+			}
+			current = child;
+		}
+
+	}
+
+	static class ComparebyName implements Comparator<Node> {
+		public int compare(Node n1, Node n2) {
+			return n1.name.compareTo(n2.name);
+		}
+
+	}
+
+	static void display(int level, Node node) {
+		System.out.print(node.name.indent(level * 2));
+		for (Node child : node.children) {
+			display(level + 1, child);
+		}
+	}
+
+	public static Node getNode(String name) {
+		for (Node node : TreeFile.roots) {
+			if (node.name.equals(name)) {
+				return node;
+			}
+
+		}
+		Node newNode = new Node("");
+		return newNode;
+	}
+
+	public static void deleteNode(String path,Node node) {
+		String[] parts = path.split("/");
+		Node current = node;
+		boolean found = false;
+		if (current.name.equals(parts[0])) {
+
+			// Düğümleri yol boyunca takip et
+			for (int i = 1; i < parts.length - 1; i++) {
+				String nodeName = parts[i];
+				found = false;
+
+				// Çocukları ara
+				for (Node child : current.children) {
+					if (child.name.equals(nodeName)) {
+						current = child;
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					System.out.println("Node not found");
+					return;
+				}
+
+			}
+
+			String nodeName = parts[parts.length - 1];
+			for (Node child : current.children) {
+				if (child.name.equals(nodeName)) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				System.out.println("Node not found");
+				return;
+			}
+
+		}
+
+		// Silinecek düğümün adı
+		String nodeNameToDelete = parts[parts.length - 1];
+
+		// Silinecek düğümü bul
+		Iterator<Node> iterator = current.children.iterator();
+		while (iterator.hasNext()) {
+			Node child = iterator.next();
+			if (child.name.equals(nodeNameToDelete)) {
+				// Düğümü kaldır ve işlemi bitir
+				iterator.remove();
+				System.out.println("Deleting is successful");
+				return;
+			}
+		}
+
+		// Silinecek düğüm bulunamazsa hata mesajı yazdır
+		System.out.println("Node not found");
+	}
+
+	private static Boolean isSearchNode(String name, Node node) {
+		Node current = node;
+		if (current.name.equals(name)) {
+			return true;
+		} else {
+			for (Node child : current.children) {
+				if (isSearchNode(name, child)) {
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+
+	public static void searchNode(String name, Node node) {
+		if (isSearchNode(name, node)) {
+			System.out.println("Node found. The path is: " + findPath(name, node));
+		} else {
+			System.out.println("Node not found");
+		}
+	}
+
+	public static String findPath(String fileName, Node node) {
+		// Dosya adı verilen dosyayı içeren düğümün yolunu bulmak için derinlemesine
+		// arama yapılır
+		return findPathRecursive(node, fileName, "");
+	}
+
+	private static String findPathRecursive(Node node, String fileName, String currentPath) {
+		// Geçerli düğümün adı belirtilen dosya adıyla eşleşirse, dosyanın yolu bulunmuş
+		// olur
+		if (node.name.equals(fileName)) {
+			return currentPath + node.name;
+		}
+
+		// Dosya adı verilen dosya bu düğümde bulunamazsa, alt düğümlere bakılır
+		for (Node child : node.children) {
+			// Yolun devamı olarak bu düğümün adı eklenir ve alt düğümlere doğru arama devam
+			// eder
+			String path = findPathRecursive(child, fileName, currentPath + node.name + "/");
+			// Eğer dosyanın yolunu bulmuşsak, bu yolu döndürürüz
+			if (path != null) {
+				return path;
+			}
+		}
+
+		// Dosya bu düğüm altında veya alt düğümlerde bulunamazsa null döndürülür
+		return null;
+	}
+
+	// In-order traversal: Left -> Root -> Right
+	public static void inOrderTraversal(Node node) {
+		System.out.println("Inorder traversal is not possible because of the tree is not a BST.");
+		System.out.println("The tree is ordered by alphabetical");
+	}
+
+	// Pre-order traversal: Root -> Left -> Right
+	public static void preOrderTraversal(Node node) {
+		if (node != null) {
+			System.out.println(node.name);
+			for (Node child : node.children) {
+				preOrderTraversal(child);
+			}
+		}
+	}
+
+	// Post-order traversal: Left -> Right -> Root
+	public static void postOrderTraversal(Node node) {
+		if (node != null) {
+			for (Node child : node.children) {
+				postOrderTraversal(child);
+			}
+			System.out.println(node.name);
+		}
+	}
+
+}
--- src/TreeFile/TreeFile.java
+++ src/TreeFile/TreeFile.java
@@ -4,232 +4,44 @@ import java.io.*;
 import java.util.*;
 
 public class TreeFile {
+	static List<Node> roots = new ArrayList<Node>();
 
-	public static void main(String[] args) {
-		class Node {
-			String name =null;
-			List<Node> children;
-
-			Node(String name) {
-				this.name = name;
-				this.children = new ArrayList<Node>();
-			}
-
-			void addChildren(String childName) {
-				String[] path = childName.split("/");
-				Node current = this;
-				for (int i = 1; i < path.length; i++) {
-					String directory = path[i];
-					Node child = null;
-					for (Node existingChild : current.children) {
-						if (existingChild.name.equals(directory)) {
-							child = existingChild;
-							break;
-						}
-					}
-					if (child == null) {
-						child = new Node(directory);
-						current.children.add(child);
-						Collections.sort(current.children, new ComparebyName());
-					}
-					current = child;
-				}
-
-			}
-
-			void display(int level) {
-				System.out.print(name.indent(level * 2));
-				for (Node child : children) {
-					child.display(level + 1);
-				}
-			}
-
-			class ComparebyName implements Comparator<Node> {
-				public int compare(Node n1, Node n2) {
-					return n1.name.compareTo(n2.name);
-				}
-
-			}
-
-			public void deleteNode(String path) {
-				String[] parts = path.split("/");
-				Node current = this;
-				boolean found = false;
-				if (current.name.equals(parts[0])) {
-
-					// Düğümleri yol boyunca takip et
-					for (int i = 1; i < parts.length - 1; i++) {
-						String nodeName = parts[i];
-						found = false;
-
-						// Çocukları ara
-						for (Node child : current.children) {
-							if (child.name.equals(nodeName)) {
-								current = child;
-								found = true;
-								break;
-							}
-						}
-						if (!found) {
-							System.out.println("Node not found");
-							return;
-						}
-
-					}
-
-					String nodeName = parts[parts.length - 1];
-					for (Node child : current.children) {
-						if (child.name.equals(nodeName)) {
-							found = true;
-							break;
-						}
-					}
-					if (!found) {
-						System.out.println("Node not found");
-						return;
-					}
-
-				}
-
-				// Silinecek düğümün adı
-				String nodeNameToDelete = parts[parts.length - 1];
-
-				// Silinecek düğümü bul
-				Iterator<Node> iterator = current.children.iterator();
-				while (iterator.hasNext()) {
-					Node child = iterator.next();
-					if (child.name.equals(nodeNameToDelete)) {
-						// Düğümü kaldır ve işlemi bitir
-						iterator.remove();
-						System.out.println("Deleting is successful");
-						return;
-					}
-				}
-
-				// Silinecek düğüm bulunamazsa hata mesajı yazdır
-				System.out.println("Node not found");
-			}
-
-			public Boolean isSearchNode(String name) {
-				Node current = this;
-				if (current.name.equals(name)) {
-					return true;
-				} else {
-					for (Node child : current.children) {
-						if (child.isSearchNode(name)) {
-							return true;
-						}
-					}
-
-				}
-				return false;
-			}
-
-			public void searchNode(String name) {
-				if (this.isSearchNode(name)) {
-					System.out.println("Node found. The path is: " + findPath(name));
-				} else {
-					System.out.println("Node not found");
-				}
-			}
-
-			public String findPath(String fileName) {
-				// Dosya adı verilen dosyayı içeren düğümün yolunu bulmak için derinlemesine
-				// arama yapılır
-				return findPathRecursive(this, fileName, "");
-			}
-
-			private String findPathRecursive(Node node, String fileName, String currentPath) {
-				// Geçerli düğümün adı belirtilen dosya adıyla eşleşirse, dosyanın yolu bulunmuş
-				// olur
-				if (node.name.equals(fileName)) {
-					return currentPath + node.name;
-				}
-
-				// Dosya adı verilen dosya bu düğümde bulunamazsa, alt düğümlere bakılır
-				for (Node child : node.children) {
-					// Yolun devamı olarak bu düğümün adı eklenir ve alt düğümlere doğru arama devam
-					// eder
-					String path = findPathRecursive(child, fileName, currentPath + node.name + "/");
-					// Eğer dosyanın yolunu bulmuşsak, bu yolu döndürürüz
-					if (path != null) {
-						return path;
-					}
-				}
-
-				// Dosya bu düğüm altında veya alt düğümlerde bulunamazsa null döndürülür
-				return null;
-			}
-
-			// In-order traversal: Left -> Root -> Right
-			public void inOrderTraversal(Node node) {
-				System.out.println("Inorder traversal is not possible because of the tree is not a BST.");
-				System.out.println("The tree is ordered by alphabetical");
-			}
-
-			// Pre-order traversal: Root -> Left -> Right
-			public void preOrderTraversal(Node node) {
-				if (node != null) {
-					System.out.println(node.name);
-					for (Node child : node.children) {
-						preOrderTraversal(child);
-					}
-				}
-			}
-
-			// Post-order traversal: Left -> Right -> Root
-			public void postOrderTraversal(Node node) {
-				if (node != null) {
-					for (Node child : node.children) {
-						postOrderTraversal(child);
-					}
-					System.out.println(node.name);
-				}
-			}
-
-		}
-
-		// main
-		List<Node> roots = new ArrayList<Node>();
-		try {
-            BufferedReader reader = new BufferedReader(new FileReader("veriler.txt"));
-            String line;
-            Node root = null;
-            while ((line = reader.readLine()) != null) {
-            	String[] path = line.split("/");
-            	if(root==null) {
-            		root=new Node(path[0]);
-            	}
-            	else if(!root.name.equals(path[0])) {
-            		root =new Node(path[0]);
-            	}
-            	else {
-            		
-                }
-            	
-            	for (int i = 1; i < path.length; i++) {
-					String nodeName = path[i];
-					root.addChildren(nodeName);
-            	}
-            	
-            	roots.add(root);
-            	
-            	}
-            	
-            	
-                
-            
-            for(Node n : roots) {
-            	n.display(0);
+	public static Node singletion(String name) {
+		for (Node node : roots) {
+            if (node.name.equals(name)) {
+                return node;
             }
-            
-            reader.close();
-        } catch (IOException e) {
-            System.err.println("Dosya okuma hatası: " + e.getMessage());
         }
 		
-		
-		
+        Node newNode = new Node(name);
+        roots.add(newNode);
+        return newNode;
+
+	}
+
+	public static void main(String[] args) {
+
+		// main
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("veriler.txt"));
+			String line;
+			Node root = null;
+			while ((line = reader.readLine()) != null) {
+				String[] path = line.split("/");
+				root =singletion(path[0]);
+
+				
+					Node.addChildren(line,root);
+				
+
+			}
+
+			for (Node n : roots) {
+				Node.display(0,n);
+			}
+
+			reader.close();
 		
 
 		Scanner scanner = new Scanner(System.in);
@@ -237,8 +49,8 @@ public class TreeFile {
 		System.out.println("Enter the path to deleting file: ");
 		String path = scanner.nextLine();
 
-//		root.deleteNode(path);
-//		root.display(0);
+		Node.deleteNode(path,root);
+		Node.display(0,root);
 
 		System.out.println("Select traversal method:");
 		System.out.println("1. In-Order");
@@ -247,25 +59,28 @@ public class TreeFile {
 		int traversalType = scanner.nextInt();
 		scanner.nextLine();
 
-//		switch (traversalType) {
-//		case 1:
-//			System.out.println("In-order traversal:");
-//			root.inOrderTraversal(root);
-//			break;
-//		case 2:
-//			System.out.println("Pre-order traversal:");
-//			root.preOrderTraversal(root);
-//			break;
-//		case 3:
-//			System.out.println("Post-order traversal:");
-//			root.postOrderTraversal(root);
-//			break;
-//		default:
-//			System.out.println("Invalid traversal type.");
-//			break;
-//		}
+		switch (traversalType) {
+		case 1:
+			System.out.println("In-order traversal:");
+			Node.inOrderTraversal(root);
+			break;
+		case 2:
+			System.out.println("Pre-order traversal:");
+			Node.preOrderTraversal(root);
+			break;
+		case 3:
+			System.out.println("Post-order traversal:");
+			Node.postOrderTraversal(root);
+			break;
+		default:
+			System.out.println("Invalid traversal type.");
+			break;
+		}
 
 		scanner.close();
+		} catch (IOException e) {
+			System.err.println("Dosya okuma hatası: " + e.getMessage());
+		}
 
 	}
 
